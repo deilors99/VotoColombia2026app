09489ac89468dfd48018bd12154e30456b982e02
deilors99 <deilors99@hotmail.com>
2025-12-25 00:18:22 -0500

    Update VotoColombia2026app.py

--- VotoColombia2026app.py
+++ VotoColombia2026app.py
@@ -186,7 +186,7 @@ st.markdown("""
 
 st.markdown("""
 <div class="background-carousel">
-    <img src="https://images.unsplash.com/photo-1529107386315-e1a2ed48a620">
+    <img src="img src="imágenes/gustavo_petro.png">
     <img src="https://images.unsplash.com/photo-1581090700227-1e37b190418e">
     <img src="https://images.unsplash.com/photo-1541873676-a18131494184">
 ">
@@ -746,6 +746,7 @@ st.markdown("""
     <p>🇨🇴 Encuesta no oficial • Consulta fuentes oficiales arriba</p>
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
