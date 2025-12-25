6c45d18c1cd5ce8d94813fdd5448adcc3828ef95
deilors99 <deilors99@hotmail.com>
2025-12-24 21:42:36 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- VotoColombia2026app.py
+++ VotoColombia2026app.py
@@ -186,7 +186,7 @@ st.markdown("""
 
 st.markdown("""
 <div class="background-carousel">
-    <img src="https://images.unsplash.com/photo-1581090700227-1e37b190418e">
+    <img src="https://www.presidencia.gov.co/prensa/galerias/Paginas/Galeria-Desfile-de-la-Independencia-Nacional-OrgulloColombiano-240720.aspx">
     <img src="https://images.unsplash.com/photo-1541873676-a18131494184">
     <img src="https://images.unsplash.com/photo-1529107386315-e1a2ed48a620">
 </div>
@@ -745,6 +745,7 @@ st.markdown("""
     <p>🇨🇴 Encuesta no oficial • Consulta fuentes oficiales arriba</p>
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
