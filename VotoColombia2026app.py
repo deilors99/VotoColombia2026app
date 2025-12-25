2794633675e866b2640eacee79de0e1b08b04ca4
deilors99 <deilors99@hotmail.com>
2025-12-24 22:15:56 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- VotoColombia2026app.py
+++ VotoColombia2026app.py
@@ -186,7 +186,7 @@ st.markdown("""
 
 st.markdown("""
 <div class="background-carousel">
-    <img src="https://www.presidencia.gov.co/GALMULTIMEDIAACTIVOS/240720-02-DESFILE-MILITAR-20-DE-JULIO-1280.JPG">
+    <img src="[[File:Presidente Gustavo Petro en 2023.jpg|Presidente_Gustavo_Petro_en_2023]]">
     <img src="https://images.unsplash.com/photo-1541873676-a18131494184">
     <img src="https://images.unsplash.com/photo-1529107386315-e1a2ed48a620">
 </div>
@@ -745,4 +745,5 @@ st.markdown("""
     <p>🇨🇴 Encuesta no oficial • Consulta fuentes oficiales arriba</p>
 </div>
 """, unsafe_allow_html=True)
+
 
